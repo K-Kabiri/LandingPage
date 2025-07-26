29462669d03dc6f56795df81ec208664ca2bd491
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2025-07-26 10:53:09 +0330

    fix(banner): change sx to tailwind.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Header from "./components/Header.jsx";
 import Banner from "./components/Banner.jsx";
 import Features from "./components/Features.jsx";
+import About from "./components/About.jsx";
 
 function App() {
 
   return (
     <>
-        <div className="App">
+        <div className="App max-w-screen mx-auto" >
             <Header />
             <Banner />
             <Features />
+            <About />
         </div>
     </>
   )
